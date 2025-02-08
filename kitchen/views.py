5afe1d626d16e9320cfd9907ba2b0e1bd3226fb1
Ihor Halytskiy <galytskiyihor@gmail.com>
2025-02-08 22:21:08 +0200

    update paginations in kitchen info

--- kitchen/views.py
+++ kitchen/views.py
@@ -89,7 +89,7 @@ def kitchen_info(request):
     dishes_list = Dish.objects.prefetch_related(
         "cooks", "ingredients", "dish_type"
     ).order_by("name")
-    paginator = Paginator(dishes_list, 5)
+    paginator = Paginator(dishes_list, 6)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
